ae2de5733201e9a6e03cf1b727001fcfec7f41d0
Igor Popov <popov.nirvana@gmail.com>
2024-11-23 16:55:46 +0300

    + decnumber

---
+++ ports/decnumber/portfile.cmake
@@ -0,0 +1,45 @@
+vcpkg_check_linkage(ONLY_STATIC_LIBRARY)
+
+find_program(GIT git)
+
+set(GIT_URL "https://github.com/nirvanaos/decNumber.cmake.git")
+set(BRANCH "master")
+set(COMMIT "a3ce7c56b17722e37cdda53d4abc4c03150737db")
+
+set(SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
+
+if(NOT EXISTS "${SOURCE_PATH}/.git")
+	message(STATUS "Cloning and fetching submodules")
+	vcpkg_execute_required_process(
+	  COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
+	  WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+	  LOGNAME clone
+	)
+endif()
+
+message(STATUS "Pull")
+vcpkg_execute_required_process(
+  COMMAND ${GIT} pull origin ${BRANCH}
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME pull
+)
+
+message(STATUS "Checkout ${COMMIT}")
+vcpkg_execute_required_process(
+  COMMAND ${GIT} checkout ${COMMIT}
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME checkout
+)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install()
+
+vcpkg_cmake_config_fixup(PACKAGE_NAME ${PORT})
+
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+file(INSTALL "${SOURCE_PATH}/decNumber/ICU-license.html" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright.html)
+configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
