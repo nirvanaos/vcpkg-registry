a19c79b62f3c804d7bf9d7fa2261761fdd5d8e93
Igor Popov <popov.nirvana@gmail.com>
2025-08-16 06:49:12 +0300

    + mockhost package

---
+++ ports/mockhost/portfile.cmake
@@ -0,0 +1,51 @@
+vcpkg_check_linkage (ONLY_DYNAMIC_LIBRARY)
+
+find_program (GIT git)
+
+set (GIT_URL "https://github.com/nirvanaos/mock-host.git")
+set (BRANCH "develop")
+set (COMMIT "4e3f80fc748f40df5c989b5c4c3eb5f86cbb9af4")
+
+set (SOURCE_PATH ${CURRENT_BUILDTREES_DIR}/src/${PORT})
+
+if (NOT EXISTS "${SOURCE_PATH}/.git")
+	message(STATUS "Cloning and fetching submodules")
+	vcpkg_execute_required_process(
+	  COMMAND ${GIT} clone --recurse-submodules ${GIT_URL} ${SOURCE_PATH}
+	  WORKING_DIRECTORY ${CURRENT_BUILDTREES_DIR}
+	  LOGNAME clone
+	)
+endif ()
+
+message (STATUS "Pull")
+vcpkg_execute_required_process (
+  COMMAND ${GIT} pull origin ${BRANCH}
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME pull
+)
+
+message (STATUS "Checkout")
+vcpkg_execute_required_process (
+  COMMAND ${GIT} checkout ${COMMIT}
+  WORKING_DIRECTORY ${SOURCE_PATH}
+  LOGNAME checkout
+)
+
+#vcpkg_from_github(
+#    OUT_SOURCE_PATH SOURCE_PATH
+#    REPO nirvanaos/idlfe
+#    HEAD_REF master
+#)
+
+vcpkg_cmake_configure (
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
+vcpkg_cmake_install ()
+
+vcpkg_cmake_config_fixup (PACKAGE_NAME "mockhost")
+
+file (REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+
+#file(INSTALL "${SOURCE_PATH}/license-lgpl.md" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
+configure_file ("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
